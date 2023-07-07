aa948c2bcf0f8bfa09939f765f85e94664a16823
Iurii Kogan <koganiurii@gmail.com>
2023-07-07 06:16:30 +0300

    update comments

--- deploy-k8s/setEnv.sh
+++ deploy-k8s/setEnv.sh
@@ -6,4 +6,4 @@ export IMAGE_NAME="iuriikogan/k8sfilechurner:v1"
 export APP_SIZE_OF_PVC_GB=30
 export APP_SIZE_OF_FILES_MB=99
 export APP_CHURN_PERCENTAGE=0.2 # this value should be a float64 representing % i.e 0.2 = 20%
-export APP_CHURN_INTERVAL_MINUTES="1m" # this should be a duration in min as string i.e. "*m"
+export APP_CHURN_INTERVAL_MINUTES="15m" # this should be a duration in min as string i.e. "*m"
