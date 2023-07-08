2093277f3df8a4ce6ffdaee2aa2cfbfbdeed4153
Iurii Kogan <koganiurii@gmail.com>
2023-07-08 14:37:28 +0300

    updated comments/defaults

--- deploy-k8s/setEnv.sh
+++ deploy-k8s/setEnv.sh
@@ -3,7 +3,7 @@ export NAMESPACE_PREFIX="test"
 export STORAGE_CLASS_NAME="managed-premium" # only RWO Storage classes
 export NUM_PVC_PER_NS=3
 export IMAGE_NAME="iuriikogan/k8sfilechurner:v1"
-export APP_SIZE_OF_PVC_GB=10
+export APP_SIZE_OF_PVC_GB=30
 export APP_SIZE_OF_FILES_MB=99
 export APP_CHURN_PERCENTAGE=0.2 # this value should be a float64 representing % i.e 0.2 = 20%
 export APP_CHURN_INTERVAL_MINUTES="15m" # this should be a duration in min as string i.e. "*m"
