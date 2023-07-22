b37c9c82bab27bca4425960fd670fcd12ce53375
Iurii Kogan <koganiurii@gmail.com>
2023-07-22 14:50:35 +0300

    profiling

--- utils/deleteTestFiles.sh
+++ utils/deleteTestFiles.sh
@@ -1,6 +1,6 @@
-## create files_to_delete by matching ls app/testfiles/ | grep for files ending in .txt
+## create files_to_delete by matching ls app/testfiles/ | grep for files ending in .bin
 ## then for loop through the files and deletes them
-files_to_delete=$(ls app/testfiles/ | grep .txt)
+files_to_delete=$(ls app/testfiles/ | grep .bin)
 
 for file in $files_to_delete; do
   rm app/testfiles/$file
